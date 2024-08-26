27c72f7e6c6573505fc0fc3cf369cfe262ac943a
Israel <israelbonicenha2020@gmail.com>
2024-08-26 07:27:37 -0300

    :sparkles: feat: criação da validação da requisição

--- OfflineCounter/OfflineCounter/uClass/ManagerValidator.cs
+++ OfflineCounter/OfflineCounter/uClass/ManagerValidator.cs
@@ -16,11 +16,31 @@
                 Util util = new Util();
 
                 int statuscode = util.RequestInternet();
+
+                Validate_Request(statuscode);
             }
             
             if(tecla_press.Key == ConsoleKey.Escape)
             {
                 Environment.Exit(0);
+            }
+        }
+
+        private void Validate_Request(int statuscode)
+        {
+            if (statuscode == 200)
+            {
+                // Sucesso
+            }
+
+            if (statuscode == 0) 
+            {
+                // Fracasso
+            }
+
+            if(statuscode != 0 &&  statuscode != 200)
+            {
+                // Não faço a mínima ideia, mas se houve resposta é porque está funcionando
             }
         }
     }
